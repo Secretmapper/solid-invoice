883d9a6df62d61c77dc2fe254ab1c0343c4b8f7f
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-08-07 17:58:32 +0800

    Fix Invoice smoke test

--- src/components/pages/Invoice/index.test.js
+++ src/components/pages/Invoice/index.test.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import initialState from '../../../redux/activeInvoice/__fixtures__/initialState'
 
 import Invoice from './index'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Invoice />, div)
+
+  ReactDOM.render(
+    <Invoice
+      {...initialState}
+      activeInvoiceChangeField={_ => {}}
+    />,
+    div
+  )
 })
